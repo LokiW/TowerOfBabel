3173bbeb7bf9fd921f40c8a1f99226a3105c04ef
LokiW <loki.w.white@gmail.com>
2021-01-30 23:57:42 -0800

    Clicking the skill button allows player to use skill, saves for soft logout but not after closing and re-opening the client completely.

--- src/main/java/com/towerofbabel/towerofbabelmod/TOBPlayerProps.java
+++ src/main/java/com/towerofbabel/towerofbabelmod/TOBPlayerProps.java
@@ -11,57 +11,86 @@ import com.towerofbabel.towerofbabelmod.babel.SkillCache;
 import com.towerofbabel.towerofbabelmod.babel.Bonuses;
 import com.towerofbabel.towerofbabelmod.tower.SkillTree;
 
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
+
 public class TOBPlayerProps {
-	
+	public static Map<Long, TOBPlayerProps> properites = new HashMap<Long, TOBPlayerProps>();
+
+	public List<String> unlockedSkills = new ArrayList<String>();
+	public Map<Bonuses, Double> stats = new HashMap<Bonuses, Double>();
+
 	public static final String PROP_NAME = TowerOfBabel.MODID + "_PlayerProps";
-	
 	public static final String SKILL_LST = TowerOfBabel.MODID + "_UNLOCKED_SKILLS";
 	public static final String PLAYER_STATS = TowerOfBabel.MODID + "_PLAYER_STATS";
-	//public List<String> unlockedSkills;
-	//public Map<Bonuses, Double> stats;
 	//TODO add event handlers to recalculate bonuses on conidtionals
 
 	public static void register() {
 		MinecraftForge.EVENT_BUS.register(new PlayerJoinHandler());
 	}
 
-	public static NBTTagCompound get(EntityPlayer e) {
-		NBTTagCompound props = e.getEntityData().getCompoundTag(PROP_NAME);
+	public static TOBPlayerProps get(EntityPlayer p) {
+		TOBPlayerProps props = TOBPlayerProps.properites.get(p.getUniqueID().getLeastSignificantBits());
+
 		if (props == null) {
-			props = new NBTTagCompound();
-			e.getEntityData().setTag(PROP_NAME, props);
+			props = new TOBPlayerProps();
+			TOBPlayerProps.properites.put(p.getUniqueID().getLeastSignificantBits(), props);
 		}
 
 		return props;
 	}
 
-	public static void addSkill(EntityPlayer p, String skillId) {
-		NBTTagCompound props = TOBPlayerProps.get(p);
-		NBTTagCompound unlockedSkills = props.getCompoundTag(SKILL_LST);
-		if (unlockedSkills == null) {
-			unlockedSkills = new NBTTagCompound();
-			props.setTag(SKILL_LST, unlockedSkills);
+	public TOBPlayerProps readFromNBT(NBTTagCompound props) {	
+		if (props == null) return this;
+		
+		NBTTagCompound nbtSkills = props.getCompoundTag(SKILL_LST);
+		if (nbtSkills != null) {
+			for (String s : nbtSkills.getKeySet()) {
+				unlockedSkills.add(s);				
+			}
+		}
+
+		NBTTagCompound nbtStats = props.getCompoundTag(PLAYER_STATS);
+		if (nbtStats != null) {
+			for (String s : nbtStats.getKeySet()) {
+				stats.put(Bonuses.valueOf(s), nbtStats.getDouble(s));
+			}
 		}
 		
-		unlockedSkills.setString(skillId, skillId);
+		return this;
+	}
+
+	public NBTTagCompound writeToNBT() {
+		NBTTagCompound nbtSkills = new NBTTagCompound();
+		for (String s : unlockedSkills) {
+			nbtSkills.setString(s,s);
+		}
+
+		NBTTagCompound nbtStats = new NBTTagCompound();
+		for (Bonuses b : stats.keySet()) {
+			nbtStats.setDouble(b.toString(), stats.get(b.toString()));
+		}
+		
+		NBTTagCompound nbtProps = new NBTTagCompound();
+		nbtProps.setTag(SKILL_LST, nbtSkills);
+		nbtProps.setTag(PLAYER_STATS, nbtStats);
+
+		return nbtProps;
+	}
+
+	public static void addSkill(EntityPlayer p, String skillId) {
+		TOBPlayerProps props = TOBPlayerProps.get(p);
+	
+		props.unlockedSkills.add(skillId);
 	}
 
 	public static void updateSkillValues(EntityPlayer p) {
-		NBTTagCompound props = TOBPlayerProps.get(p);
+		TOBPlayerProps props = TOBPlayerProps.get(p);
 		SkillCache.clearPlayerCache(p.getUniqueID());
 
-		NBTTagCompound unlockedSkills = props.getCompoundTag(SKILL_LST);
-		if (unlockedSkills == null) {
-			unlockedSkills = new NBTTagCompound();
-			props.setTag(SKILL_LST, unlockedSkills);
-		}
-
-		NBTTagCompound stats = props.getCompoundTag(PLAYER_STATS);
-		if (stats == null) {
-			stats = new NBTTagCompound();
-			props.setTag(PLAYER_STATS, stats);
-		}
-		for (String s : unlockedSkills.getKeySet()) {
+		for (String s : props.unlockedSkills) {
 			SkillTree skill = TowerOfBabel.skills.get(s);
 
 			for (Actions a : skill.permissions.keySet()) {
@@ -69,7 +98,7 @@ public class TOBPlayerProps {
 			}
 
 			for (Bonuses b : skill.bonuses.keySet()) {
-				stats.setDouble(b.toString(), Bonuses.resolveBonus(b, skill.bonuses.get(b), stats.getDouble(b.toString())));
+				props.stats.put(b, Bonuses.resolveBonus(b, skill.bonuses.get(b), props.stats.get(b)));
 			}
 		}
 	}
@@ -84,7 +113,19 @@ public class TOBPlayerProps {
 		 */
 		@SubscribeEvent
 		public void playerLogin(PlayerEvent.PlayerLoggedInEvent e) {
+			TOBPlayerProps.get(e.player).readFromNBT(e.player.getEntityData().getCompoundTag(PROP_NAME));
 			TOBPlayerProps.updateSkillValues(e.player);
+		}
+
+		/*
+		 * Save properties for data when they logout
+		 */
+		@SubscribeEvent
+		public void playerLogout(PlayerEvent.PlayerLoggedOutEvent e){
+			NBTTagCompound nbtProps = TOBPlayerProps.get(e.player).writeToNBT();
+			NBTTagCompound base = e.player.getEntityData();
+			base.setTag(PROP_NAME, nbtProps);
+			e.player.writeToNBT(base);
 		}
 
 	}
